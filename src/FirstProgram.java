09252f1af76facf235c5d6e6141f868f20660772
Andrii <amudrevskiy@gmail.com>
2017-01-24 20:21:20 +0200

    first commit

---
+++ src/FirstProgram.java
@@ -0,0 +1,10 @@
+/**
+ * Created by andrii on 1/24/17.
+ */
+public class FirstProgram {
+    public static void main(String[] args) {
+        System.out.println("first program works");
+
+        System.out.println("done...");
+    }
+}
---
+++ src/Test.java
@@ -0,0 +1,11 @@
+/**
+ * Created by andrii on 1/24/17.
+ */
+public class Test {
+
+    public static void main(String[] args) {
+
+
+
+    }
+}
